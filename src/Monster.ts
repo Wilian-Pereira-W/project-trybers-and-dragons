276f26a4f4266308129a1b4c2249485db489eebb
Wilian Pereira <wilian_pereira92@outlook.com>
2022-08-29 20:30:27 -0300

    feat: implementado SimpleFighter

---
+++ src/Monster.ts
@@ -0,0 +1,39 @@
+import Fighter, { SimpleFighter } from './Fighter';
+
+class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor(lifePoints = 85, strength = 63) {
+    this._lifePoints = lifePoints;
+    this._strength = strength;
+  }
+
+  get lifePoints(): number {
+    return this._lifePoints;
+  }
+
+  get strength(): number {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number | void {
+    const damage = attackPoints;
+    if (this._lifePoints === -1) {
+      return this._lifePoints;
+    }
+    if (damage > 0) {
+      this._lifePoints -= damage;
+    }
+    if (this.lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+    return this._lifePoints;
+  }
+
+  attack(enemy: Fighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
+
+export default Monster;
